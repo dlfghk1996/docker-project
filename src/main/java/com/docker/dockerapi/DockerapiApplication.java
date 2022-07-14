340bd7c4e1442f0dd19c0b51ac0fc364f589bcff
ilhwakim <1hwazz@mommaeat.com>
2022-07-14 17:30:30 +0900

    main api 생성

---
+++ src/main/java/com/docker/dockerapi/DockerapiApplication.java
@@ -0,0 +1,13 @@
+package com.docker.dockerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/docker/dockerapi/DockerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.dockerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
